961d4e48a6df600df1e738c60f8dbf2c88cc8718
Dillon Lee <crazycapn@gmail.com>
2015-08-03 13:51:17 -0700

    added constructors, function

--- car.php
+++ car.php
@@ -1,30 +1,20 @@
 <?php
   class Car
   {
-    public $make_model;
-    public $price;
-    public $miles;
+
+    function __construct($make_model, $price, $miles) {
+      $this->make_model = $make_model;
+      $this->price = $price;
+      $this->miles = $miles;
+    }
   }
 
-  $porsche = new Car();
-  $porsche->make_model = "2014 porsche 911";
-  $porsche->price = 114991;
-  $porsche->miles = 7864;
+  $porsche = new Car("2014 Porsche 911", 114991, 7864);
+  $ford = new Car("2011 Ford F450", 55995, 14214);
+  $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+  $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
-  $ford = new Car();
-  $ford->make_model = "2011 Ford F450";
-  $ford->price = 55995;
-  $ford->miles = 14241;
 
-  $lexus = new Car();
-  $lexus->make_model = "2013 Lexus RX 350";
-  $lexus->price = 44700;
-  $lexus->miles = 20000;
-
-  $mercedes = new Car();
-  $mercedes->make_model = "Mercedes Benz CLS550";
-  $mercedes->price = 39900;
-  $mercedes->miles = 37979;
 
   $cars = array($porsche, $ford, $lexus, $mercedes);
 
