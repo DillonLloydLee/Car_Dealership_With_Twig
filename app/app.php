6c6db9f357dc1f58dc52a290e251a932a2c52f8a
Dillon Lee <crazycapn@gmail.com>
2015-08-04 16:42:41 -0700

    Fixed the code so that it works!  Damn.  Did that by myself in the last 20 minutes of class.  Feels good.

--- app/app.php
+++ app/app.php
@@ -36,10 +36,10 @@
   });
 
   $app->get("carfinal", function() {
-    $porsche = new Car("2014 Porsche 911", 114991, 7864, "/../images/porsche.jpg");
-    $ford = new Car("2011 Ford F450", 55995, 14214, "/../images/ford.jpg");
-    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "/../images/lexus.jpg");
-    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/../images/mercedes.jpg");
+    $porsche = new Car("2014 Porsche 911", 114991, 7864, "/images/porsche.jpg");
+    $ford = new Car("2011 Ford F450", 55995, 14214, "/images/ford.jpg");
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "/images/lexus.jpg");
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "/images/mercedes.jpg");
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
     $cars_we_want = array();
@@ -51,18 +51,43 @@
     }
 
     $output = "";
-    if ($cars_we_want == array())
-      $output = $output .
+
+    $output = $output .
       "<head>
         <title>Your Car Dealership's Homepage</title>
       </head>
       <body>
           <h1>Your Car Dealership</h1>
-          <h2>Sorry, no cars available</h2>
       </body>
       </html>";
 
+    if ($cars_we_want == array()) {
+      $output = $output .
+      "<h2>Sorry, no cars available<h2>
+      </body>
+      </html>";
+    }
 
+
+
+    foreach ($cars_we_want as $car) {
+      $carModel = $car->getModel();
+      $carPrice = $car->getPrice();
+      $carMiles = $car->getMiles();
+      $output = $output .
+        "<li> $carModel </li>
+        <ul>
+            <img src='$car->image' height=100 width=200>
+            <li> $$carPrice </li>
+            <li> Miles: $carMiles </li>
+          </ul>
+          <li></li>
+        ";
+      }
+
+      return $output;
+
+      ;
   });
 
   return $app;
